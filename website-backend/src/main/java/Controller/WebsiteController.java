143f6a726b4d131bd3f9ef25e07666a26e1877f9
alstonStone <alstonStone0@gmail.com>
2023-06-15 14:03:49 -0400

    working on sending email

--- website-backend/src/main/java/Controller/WebsiteController.java
+++ website-backend/src/main/java/Controller/WebsiteController.java
@@ -1,12 +1,23 @@
 package Controller;
 
-import io.javalin.Javalin;
-import org.eclipse.jetty.http.HttpTester;
-import io.javalin.http.Context;
-import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
-import java.io.IOException;
+import Model.EmailAdress;
+import Util.EmailSender;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import org.apache.commons.mail.*;
+import io.javalin.Javalin;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.SimpleEmail;
+import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.post;
+import static j2html.TagCreator.br;
+import static j2html.TagCreator.button;
+import static j2html.TagCreator.form;
+import static j2html.TagCreator.input;
+import static j2html.TagCreator.textarea;
 
 public class WebsiteController {
 
@@ -35,9 +46,26 @@ public class WebsiteController {
 
 
 
-    private void postContactHandler(Context context) {
+    private void postContactHandler(Context context) throws JsonProcessingException, EmailException {
         context.result("post called"+context.body());
-        System.out.println("it worked");
+        ObjectMapper mapper = new ObjectMapper();
+        EmailAdress emailAdress = mapper.readValue(context.body(), EmailAdress .class);
+
+        System.out.println(emailAdress.getEmail());
+
+
+
+
+        Email email = new SimpleEmail();
+        email.setHostName("smtp.googlemail.com");
+        email.setSmtpPort(465);
+        email.setAuthenticator(new DefaultAuthenticator("alstonstone0@gmail.com", "tgjsjqwefpfnhzlv"));
+        email.setSSLOnConnect(true);
+        email.setFrom("alstonstone0@gmail.com");
+
+        email.setMsg("message");
+        email.addTo(emailAdress.getEmail());
+        email.send(); // will throw email-exception if something is wrong
 
     }
 
---
+++ website-backend/src/main/java/Model/EmailAdress.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class EmailAdress {
+
+    private String email;
+
+    public EmailAdress(){
+
+    }
+     public String getEmail(){
+        return this.email;
+     }
+
+     public void setEmail(String newEmail){
+        this.email = newEmail;
+     }
+}
